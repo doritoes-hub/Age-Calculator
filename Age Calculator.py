89cfcfff51c17a09654894da640ee4161b32047b
doritoes-hub <sanviwagadre@gmail.com>
2026-06-15 15:37:18 +0530

    Update Age Calculator.py

--- Age Calculator.py
+++ Age Calculator.py
@@ -1,14 +1,8 @@
 import streamlit as st
-st.markdown(
-    """
-    <style>
-    #MainMenu {visibility: hidden;}
-    header {visibility: hidden;}
-    footer {visibility: hidden;}
-    .stAppDeployButton {display: none !important;}
-    </style>
-    """,
-    unsafe_html=True)
+st.set_page_config(layout="wide") # Optional: keeps your layout nice and wide
+
+# This completely hides the header, footer, and deploy button
+st.markdown("<style>header {visibility: hidden;} footer {visibility: hidden;} .stAppDeployButton {display: none !important;}</style>", unsafe_html=True))
 
 def add_side_doodles(left_file, right_file):
     import base64
